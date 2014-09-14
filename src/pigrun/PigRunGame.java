69e235fa108531c9ae8b3a55af96320c5fd86689
Thanathip Lertprasitphan <green.gie1234@gmail.com>
2014-09-14 20:10:29 +0700

    set width,height

---
+++ src/pigrun/PigRunGame.java
@@ -0,0 +1,50 @@
+package pigrun;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+
+
+public class PigRunGame extends BasicGame {
+
+	private static final int Game_Width = 800;
+	private static final int Game_Height = 600;
+
+	public PigRunGame(String title) {
+		super(title);
+		
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		
+		
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+		
+		
+	}
+
+	@Override
+	public void update(GameContainer container, int delta) throws SlickException {
+		
+	}
+	
+	public static void main(String[] args) {
+	    try {
+	      PigRunGame game = new PigRunGame("PigRun Game");
+	      AppGameContainer appgc = new AppGameContainer(game);
+	      appgc.setDisplayMode(Game_Width, Game_Height, false);
+	      appgc.setMinimumLogicUpdateInterval(1000 / 50);
+	      appgc.start();
+	    } catch (SlickException e) {
+	      e.printStackTrace();
+	    }
+	  }
+
+}
